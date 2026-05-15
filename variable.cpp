02734de0c9bca405d7201a4c5852b87ddb0344cd
s1140460-stack <s1140460@dmhs.kh.edu.tw>
2026-05-15 11:38:18 +0800

    Create variable.cpp

---
+++ variable.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+useing namespace std;
+
+int main() {
+	int age;
+	string name;
+	cout << "請輸入年齡" << end1; 
+	cin >> age;
+	cout << "請輸入名字" << end1;
+	cin >> name;
+	cout << "你是" << name << "今年" << age << "歲" << end1;
+} 
